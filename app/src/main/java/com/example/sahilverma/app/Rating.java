33683879ef573775e6f8bd3640372617d9e51625
Sahil Verma <v.sahil1@gmail.com>
2016-10-01 22:15:06 +0530

    Second Commit

---
+++ app/src/main/java/com/example/sahilverma/app/Rating.java
@@ -0,0 +1,16 @@
+package com.example.sahilverma.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sahilverma on 01/10/16.
+ */
+
+public class Rating  extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating);
+    }
+}
